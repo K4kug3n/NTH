548efe97a1a21dffb27ec94a6034b26186bc94e1
K4kug3n <maxime.rambosson@gmail.com>
2022-06-30 01:09:41 +0200

    Initial commit

---
+++ xmake.lua
@@ -0,0 +1,17 @@
+set_project("NTH")
+add_requires("vulkan-memory-allocator", "vulkan-headers", "libsdl", "tinyobjloader", "stb")
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.vsxmake.autoupdate")
+
+add_includedirs("include")
+add_files("src/**.cpp")
+
+set_languages("cxx17")
+set_warnings("allextra")
+
+target("basic_render")
+	add_defines("VK_NO_PROTOTYPES")
+
+	add_files("exemples/basic_render/main.cpp")
+
+	add_packages("vulkan-memory-allocator", "vulkan-headers", "libsdl", "tinyobjloader", "stb")

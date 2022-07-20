e6bf07fc1512b100acad3bd56afac834b9160f5c
K4kug3n <kakugen@yahoo.fr>
2022-07-21 01:35:34 +0200

    Load mesh

--- src/Renderer/Mesh.cpp
+++ src/Renderer/Mesh.cpp
@@ -27,7 +27,7 @@ namespace Nth {
 
 				vertex.texturePos = {
 					attrib.texcoords[2 * index.texcoord_index + 0],
-					attrib.texcoords[2 * index.texcoord_index + 1]
+					1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
 				};
 
 				vertex.color = { 1.0f, 1.0f, 1.0f };

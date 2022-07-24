9bb61819c92680adc94abdc0ca94b65025c93e7d
K4kug3n <maxime.rambosson@gmail.com>
2022-07-25 01:41:18 +0200

    Move pipeline creation to material

--- include/Renderer/Material.hpp
+++ include/Renderer/Material.hpp
@@ -1,12 +1,17 @@
-#pragma once
-
 #ifndef NTH_RENDERER_MATERIAL_HPP
 #define NTH_RENDERER_MATERIAL_HPP
 
 #include "Renderer/Vulkan/Pipeline.hpp"
 #include "Renderer/Vulkan/PipelineLayout.hpp"
+#include "Renderer/Vulkan/ShaderModule.hpp"
+#include "Renderer/Vulkan/RenderPass.hpp"
+
+#include <string>
+#include <vector>
 
 namespace Nth {
+	class Vk::Device;
+
 	class Material {
 	public:
 		Material() = default;
@@ -14,11 +19,18 @@ namespace Nth {
 		Material(Material&&) = default;
 		~Material() = default;
 
+		bool createPipeline(Vk::Device const& device, Vk::RenderPass const& renderPass, const std::string_view vertexShaderName, const std::string_view fragmentShaderName, std::vector<VkDescriptorSetLayout> const& descriptorSetLayouts);
+
 		Material& operator=(Material const&) = delete;
 		Material& operator=(Material&&) = default;
 
 		Vk::Pipeline pipeline;
 		Vk::PipelineLayout pipelineLayout;
+
+	private:
+		bool createPipelineLayout(Vk::Device const& device, std::vector<VkDescriptorSetLayout> const& descriptorSetLayouts);
+
+		Vk::ShaderModule createShaderModule(Vk::Device const& device, const std::string_view filename) const;
 	};
 }
 

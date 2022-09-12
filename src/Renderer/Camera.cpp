2fbf74be8e8f847b310cbdaae5f9399160d11906
K4kug3n <maxime.rambosson@gmail.com>
2022-09-12 19:12:14 +0200

    Add first person camera

--- src/Renderer/Camera.cpp
+++ src/Renderer/Camera.cpp
@@ -3,7 +3,16 @@
 #include <Math/Matrix4.hpp>
 
 namespace Nth {
+	Camera::Camera() :
+		position(0.f, 0.f, 0.f),
+		direction(0.f, 0.f, 0.f),
+		type(CameraType::FirstPerson) { }
+
 	Matrix4f Camera::getViewMatrix() const {
+		if (type == CameraType::FirstPerson) {
+			return direction.toRotationMatrix() * Matrix4f::Translation(position);
+		}
+
 		return Matrix4f::Translation(position) * direction.toRotationMatrix();
 	}
 }

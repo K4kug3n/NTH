ceddc423e373efe0dc7aaa8ee9fc3cd8510f4646
K4kug3n <maxime.rambosson@gmail.com>
2022-09-12 02:08:45 +0200

    Fix linux compilation

--- src/Math/AssimpConvertion.cpp
+++ src/Math/AssimpConvertion.cpp
@@ -6,10 +6,10 @@
 namespace Nth {
 	Matrix4f toMatrix4(aiMatrix4x4 const& mat) {
 		return Matrix4f{
-			mat[0][0], mat[1][0], mat[2][0], mat[3][0],
-			mat[0][1], mat[1][1], mat[2][1], mat[3][1],
-			mat[0][2], mat[1][2], mat[2][2], mat[3][2],
-			mat[0][3], mat[1][3], mat[2][3], mat[3][3],
+			mat.a1, mat.b1, mat.c1, mat.d1,
+			mat.a2, mat.b2, mat.c2, mat.d2,
+			mat.a3, mat.b3, mat.c3, mat.d3,
+			mat.a4, mat.b4, mat.c3, mat.d4,
 		};
 	}
 
